3244e280612abe47cba87ee862ae918a593bc4a9
Fredrik Carlen <obscaenvs@gmail.com>
2014-08-17 15:32:25 +0200

    Moved libdir out of app.

--- Angular.hs
+++ Angular.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE OverloadedStrings, TemplateHaskell, QuasiQuotes, FlexibleInstances, TypeFamilies, GeneralizedNewtypeDeriving #-}
-module AngularDemoTypes where
+module Angular where
 
 import           Yesod
 
